7b65366f6f9e6c6b8dd0f5e8f0611e0106c3483a
Paulina Lichota <paulinalichota1@gmail.com>
2025-04-29 21:27:12 +0200

    wip

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,17 +6,70 @@
 /*   By: plichota <plichota@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 15:54:05 by plichota          #+#    #+#             */
-/*   Updated: 2025/04/29 16:55:36 by plichota         ###   ########.fr       */
+/*   Updated: 2025/04/29 21:26:08 by plichota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
+/*
+◦ timestamp_in_ms X has taken a fork
+◦ timestamp_in_ms X is eating
+◦ timestamp_in_ms X is sleeping
+◦ timestamp_in_ms X is thinking
+◦ timestamp_in_ms X died
+*/
+
 int main(int argc, char *argv[])
 {
-  if (argc < 4 || argc > 6)
+  int number_of_philosophers;
+  int time_to_die;
+  int time_to_eat;
+  int time_to_sleep;
+  int number_of_times_each_philosopher_must_eat;
+
+  if (argc < 5 || argc > 6)
     return (printf("Wrong usage of arguments\n"), 0);
-  // dev'essere un numero
-  // non può essere < 1
-  number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]
+  
+  if (!is_number(argv[1]))
+    return (printf("Invalid first argument\n"), 0);
+  number_of_philosophers = ft_atoi(argv[1]);
+  // qual è il massimo?
+  if (number_of_philosophers <= 0)
+    return (printf("Invalid number of philosophers\n"), 0);
+
+  if (!is_number(argv[2]))
+    return (printf("Invalid second argument\n"), 0);
+  time_to_die = ft_atoi(argv[2]);
+  // può essere 0?? max?
+  if (time_to_die <= 0)
+    return (printf("Invalid time_to_die\n"), 0);
+
+  if (!is_number(argv[3]))
+    return (printf("Invalid third argument\n"), 0);
+  time_to_eat = ft_atoi(argv[3]);
+  // può essere 0?? max?
+  if (time_to_eat <= 0)
+    return (printf("Invalid time_to_eat\n"), 0);
+
+  if (!is_number(argv[4]))
+    return (printf("Invalid fourth argument\n"), 0);
+  time_to_sleep = ft_atoi(argv[4]);
+  // può essere 0?? max?
+  if (time_to_sleep <= 0)
+    return (printf("Invalid time_to_sleep\n"), 0);
+
+  if (argc == 6)
+  {
+    if (!is_number(argv[5]))
+      return (printf("Invalid fifth argument\n"), 0);
+    number_of_times_each_philosopher_must_eat = ft_atoi(argv[4]);
+    // può essere 0?? max?
+    if (number_of_times_each_philosopher_must_eat <= 0)
+      return (printf("Invalid number_of_times_each_philosopher_must_eat\n"), 0);
+  }
+  /*
+  If all philosophers have eaten at least number_of_times_each_philosopher_must_eat
+  times, the simulation stops.
+  */
 }
---
+++ philo/src/utils.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: plichota <plichota@student.42firenze.it    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 17:12:17 by plichota          #+#    #+#             */
+/*   Updated: 2025/04/29 21:19:39 by plichota         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+static int	ft_isspace(int c)
+{
+	if (c == ' ' || c == '\t' || c == '\f'
+		|| c == '\n' || c == '\v' || c == '\r')
+		return (1);
+	return (0);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	res;
+	int	sign;
+
+	sign = 1;
+	res = 0;
+	while (ft_isspace(*str))
+	{
+		str++;
+	}
+	if (*str == '+' || *str == '-')
+	{
+		if (*str == '-')
+			sign = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		res = (res * 10) + (*str - '0');
+		str++;
+	}
+	return (res * sign);
+}
+
+int is_number(char *s)
+{
+  if (!s)
+    return (0);
+  if (*s == '+' || *s == '-')
+    s++;
+  while (*s != '\0')
+  {
+    if (*s < '0' || *s > '9')
+      return (0);
+    s++;
+  }
+  return (1);
+}
