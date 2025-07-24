9a7ea782d64cd0d094849bc7f78aedcf8bc6d7fe
Paulina Lichota <plichota@c2r5p5.42firenze.it>
2025-07-24 18:25:13 +0200

    one philosopher case fixed

--- src/simulation2.c
+++ src/simulation2.c
@@ -6,7 +6,7 @@
 /*   By: plichota <plichota@student.42firenze.it    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 22:30:20 by plichota          #+#    #+#             */
-/*   Updated: 2025/07/24 17:07:24 by plichota         ###   ########.fr       */
+/*   Updated: 2025/07/24 18:23:29 by plichota         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,18 @@ void	join_threads(t_philo *philosophers, t_table *table)
 }
 
 // aspetta lentamente la sua morte con una sola forchetta in mano
+// È ora di morire? È ora di morire? È ora di morire?
 int	handle_single_philosopher(t_philo *philo)
 {
+	unsigned long long	death_time;
+
+	death_time = philo->table->start_time + philo->table->time_to_die;
 	pthread_mutex_lock(philo->fork_left);
 	safe_print(philo, "has taken a fork");
-	while (!check_table_death(philo))
-		usleep(100);
+	while (get_system_time_ms() < death_time)
+		usleep(50);
 	pthread_mutex_unlock(philo->fork_left);
-	return (0);
+	safe_print(philo, "died");
+	set_death(philo);
+	return (1);
 }
